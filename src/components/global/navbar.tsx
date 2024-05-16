1631af12547b8cebdfd2e4be83b9748ed3899206
bwcummings1 <bwcummings1@gmail.com>
2024-05-15 22:36:40 -0500

    changes to navbar:  button color, padding, 'Documentation' orientation'

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -9,7 +9,7 @@ type Props = {}
 const Navbar = async (props: Props) => {
   const user = await currentUser()
   return (
-    <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
+    <header className="fixed right-0 left-0 top-0 py-4 px-20 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
       <aside className="flex items-center gap-[8px]">
         <Image 
         src="/flowgridLogo.png" 
@@ -21,11 +21,11 @@ const Navbar = async (props: Props) => {
         <p className="text-3xl font-bold">Flowgrid</p>
       </aside>
       <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
-        <ul className="flex items-center gap-4 list-none">
+        {/* <ul className="flex items-center gap-4 list-none">
           <li>
             <Link href="#">Documentation</Link>
           </li>
-          {/* <li>
+          <li>
             <Link href="#">Pricing</Link>
           </li>
           <li>
@@ -39,16 +39,21 @@ const Navbar = async (props: Props) => {
           </li>
           <li>
             <Link href="#">Enterprise</Link>
-          </li> */}
-        </ul>
+          </li>
+        </ul> */}
       </nav>
-      <aside className="flex items-center gap-4">
+      <aside className="flex items-center gap-8">
+         <ul className="flex items-center gap-4 list-none">
+            <li>
+              <Link href="#">Documentation</Link>
+            </li>
+          </ul>
         <Link
           href="/dashboard"
           className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
         >
-          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#327EFF_0%,#FF6446_33%,#FFDE2D_67%,#327EFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-8 py-2 text-sm font-medium text-white backdrop-blur-3xl">
             {user ? 'Dashboard' : 'Get Started'}
           </span>
         </Link>
